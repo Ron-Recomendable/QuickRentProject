4dc9e010d997a12ce08e543ccde4805527b0dd39
Ron Recomendable <ac159253@avcol.school.nz>
2025-03-26 10:12:34 +1300

    Added CRUD on each Tables.

---
+++ QuickRentProject/Controllers/PaymentsController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using QuickRentProject.Models;
+using QuickRentProjectDb.Data;
+
+namespace QuickRentProject.Controllers
+{
+    public class PaymentsController : Controller
+    {
+        private readonly QuickRentProjectDbContext _context;
+
+        public PaymentsController(QuickRentProjectDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Payments
+        public async Task<IActionResult> Index()
+        {
+            var quickRentProjectDbContext = _context.Payment.Include(p => p.Booking);
+            return View(await quickRentProjectDbContext.ToListAsync());
+        }
+
+        // GET: Payments/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var payment = await _context.Payment
+                .Include(p => p.Booking)
+                .FirstOrDefaultAsync(m => m.PaymentId == id);
+            if (payment == null)
+            {
+                return NotFound();
+            }
+
+            return View(payment);
+        }
+
+        // GET: Payments/Create
+        public IActionResult Create()
+        {
+            ViewData["BookingId"] = new SelectList(_context.Booking, "BookingId", "RenterId");
+            return View();
+        }
+
+        // POST: Payments/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("PaymentId,Amount,PaymentDate,PaymentMethod,BookingId")] Payment payment)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(payment);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BookingId"] = new SelectList(_context.Booking, "BookingId", "RenterId", payment.BookingId);
+            return View(payment);
+        }
+
+        // GET: Payments/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var payment = await _context.Payment.FindAsync(id);
+            if (payment == null)
+            {
+                return NotFound();
+            }
+            ViewData["BookingId"] = new SelectList(_context.Booking, "BookingId", "RenterId", payment.BookingId);
+            return View(payment);
+        }
+
+        // POST: Payments/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("PaymentId,Amount,PaymentDate,PaymentMethod,BookingId")] Payment payment)
+        {
+            if (id != payment.PaymentId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(payment);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PaymentExists(payment.PaymentId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BookingId"] = new SelectList(_context.Booking, "BookingId", "RenterId", payment.BookingId);
+            return View(payment);
+        }
+
+        // GET: Payments/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var payment = await _context.Payment
+                .Include(p => p.Booking)
+                .FirstOrDefaultAsync(m => m.PaymentId == id);
+            if (payment == null)
+            {
+                return NotFound();
+            }
+
+            return View(payment);
+        }
+
+        // POST: Payments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var payment = await _context.Payment.FindAsync(id);
+            if (payment != null)
+            {
+                _context.Payment.Remove(payment);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PaymentExists(int id)
+        {
+            return _context.Payment.Any(e => e.PaymentId == id);
+        }
+    }
+}
