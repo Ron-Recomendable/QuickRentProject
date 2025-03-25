c93fe6991ff3221e07cfd29b23883a5b5f492ae4
Ron Recomendable <ac159253@avcol.school.nz>
2025-03-26 10:13:25 +1300

    CRUD on Migration

--- QuickRentProject/Areas/Identity/Data/QuickRentProjectDbContext.cs
+++ QuickRentProject/Areas/Identity/Data/QuickRentProjectDbContext.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using QuickRentProject.Areas.Identity.Data;
+using QuickRentProject.Models;
 
 namespace QuickRentProjectDb.Data;
 
@@ -19,4 +20,12 @@ public class QuickRentProjectDbContext : IdentityDbContext<QuickRentProjectUser>
         // For example, you can rename the ASP.NET Identity table names and more.
         // Add your customizations after calling base.OnModelCreating(builder);
     }
+
+public DbSet<QuickRentProject.Models.Item> Item { get; set; } = default!;
+
+public DbSet<QuickRentProject.Models.Booking> Booking { get; set; } = default!;
+
+public DbSet<QuickRentProject.Models.Payment> Payment { get; set; } = default!;
+
+public DbSet<QuickRentProject.Models.Review> Review { get; set; } = default!;
 }
---
+++ QuickRentProject/Migrations/20250325205043_GeneratedCRUD.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuickRentProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class GeneratedCRUD : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
